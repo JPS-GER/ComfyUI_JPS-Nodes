296b6b0bae77b90b2b7a1d229f47ae684ff23907
JPS <142158778+JPS-GER@users.noreply.github.com>
2023-10-01 21:55:07 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author: JPS
 @title: JPS Custom Nodes for ComfyUI
 @nickname: JPS Custom Nodes
-@description: Various nodes to handle SDXL Resolutions, SDXL Basic Settings, IP Adapter Settings, Revision Settings, SDXL Prompt Styler, Crop Image to Square, Crop Image to Target Sizue, Get Date-Time String, Resolution Multiply, Largest Integer, 5-to-1 Switches for Integer, Images, Latents, Conditioning, Model, VAE, ControlNet
+@description: Various nodes to handle SDXL Resolutions, SDXL Basic Settings, IP Adapter Settings, Revision Settings, SDXL Prompt Styler, Crop Image to Square, Crop Image to Target Size, Get Date-Time String, Resolution Multiply, Largest Integer, 5-to-1 Switches for Integer, Images, Latents, Conditioning, Model, VAE, ControlNet
 """
 
 from .jps_nodes import NODE_CLASS_MAPPINGS
